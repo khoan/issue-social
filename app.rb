27963c03cc5899d01fba40dafe9988dd7bb4fe8c
khoa nguyen <huukhoanguyen@hotmail.com>
2015-11-18 16:07:14 +1100

    - set Facebook id cookie when user authenticates - returns user public profile based on Facebook id cookie - move html template into files

--- app.rb
+++ app.rb
@@ -11,6 +11,13 @@ IO.read('env.sh').scan(/(.*?)="?(.*)"?$/).each do |key, value|
   ENV[key] ||= value
 end
 
+class Cuba
+  def render template_path, vars={}
+    template = Mote.parse(IO.read(template_path), self, vars.keys)
+    res.write template.call(vars)
+  end
+end
+
 Cuba.define do
   on 'facebook' do
     require_relative 'facebook'
--- facebook.rb
+++ facebook.rb
@@ -4,84 +4,28 @@
 # /auth   a demo of login flow
 # /demo   a demo of login flow within iframe 
 
-api = RestCore::Facebook.new(app_id: ENV['FACEBOOK_KEY'], secret: ENV['FACEBOOK_SECRET'])
-default_redirect_url ='http://issue-social.dev/facebook/callback'
-
 Facebook = Cuba.new do
   on 'demo' do
-    template = Mote.parse(<<HTML, self)
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Facebook iOS home screen App demo</title>
-</head>
-<body>
-  <a class="fb-connect iframe" href="#">connect via iframe</a>
-  <a class="fb-connect popup" href="#">connect via popup</a>
+    render 'template/facebook/demo.html'
+  end
 
-  <script>
-    (function init(connect, iframe) {
-      var connect = document.querySelectorAll(".fb-connect"), i;
-      for(i = 0; i < connect.length; ++i) {
-        connect[i].addEventListener("click", function(event) {
-          var classList = event.target.classList;
-          event.preventDefault();
+  api = RestCore::Facebook.new(app_id: ENV['FACEBOOK_KEY'], secret: ENV['FACEBOOK_SECRET'], log_method: Log.method(:info))
+  default_redirect_url ='http://issue-social.dev/facebook/callback'
 
-          if (classList.contains("iframe")) {
-            iframe = document.createElement("iframe");
-            iframe.src = "http://issue-social.dev/facebook/auth";
-            document.body.appendChild(iframe);
-          } else if (classList.contains("popup")) {
-            window.open("http://issue-social.dev/facebook/auth");
-          }
-        });
-      }
-
-      window.addEventListener("message", function(message) {
-        console.log('----> child says', message);
-      }, false);
-    })();
-  </script>
-</body>
-</html>
-HTML
-
-    res.write template.call
+  on 'profile' do
+    user = api.get(req.cookies['fb_id'], access_token: api.secret_access_token)
+    render 'template/facebook/profile.html', user: user
   end
 
   on 'callback' do
     token = api.authorize!(code: req.params['code'], redirect_uri: default_redirect_url)
     token['expired_at'] = (Time.now + token.delete('expires').to_i).httpdate
 
-    template = Mote.parse(<<HTML, self, [:token])
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Facebook callback</title>
-</head>
-<body>
-  % for k,v in token
-  {{k}}: {{v}}<br>
-  % end
+    debug = api.get('debug_token', input_token: token['access_token'], access_token: api.secret_access_token)
 
-  <script>
-    (function init(parent) {
-      if (window.opener) {
-        parent = window.opener;
-      } else if (window.parent) {
-        parent = window.parent;
-      }
+    res.set_cookie('fb_id', value: debug['data']['user_id'], expires: Time.now+60*60*24, path: '/', domain: '.issue-social.dev')
 
-      parent.postMessage({{JSON.dump token}}, "*");
-    })();
-  </script>
-</body>
-</html>
-HTML
-
-    res.write template.call(token: token)
+    render 'template/facebook/callback.html', token: token, debug: debug
   end
 
   on 'auth' do
